148da92e1ca1c77f82e1eb6252d8908dcc76c50e
ruseel <ruseel@gmail.com>
2022-06-23 21:07:14 +0900

    WIP

--- notebooks/mysql.clj
+++ notebooks/mysql.clj
@@ -27,14 +27,16 @@
 ;;
 ;; mysql reference manual 에서 챕터
 ;; [11.2.2 The DATE, DATETIME, and TIMESTAMP Types](https://dev.mysql.com/doc/refman/8.0/en/datetime.html) 를
-;; 아주 일부분만 설명해 보면, 이렇습니다. mysql 은 `TIMESTAMP` datatype 을 storage 에 저장할 때
-;; UTC 로 변환해 저장해 둡니다. 꺼낼 때는 time zone 을 고려해서 변환합니다.
+;; 아주 아주 일부분만 발췌하면, 이렇습니다. mysql 은 datatype `TIMESTAMP` 을 storage 에 저장할 때
+;; UTC 로 변환해 저장합니다. 꺼낼 때는 time zone 을 고려해서 변환합니다.
 ;; `DATETIME` datatype 은 변환없이 그대로 저장하고 꺼냅니다.
 ;;
 
 (java.time.ZonedDateTime/now)
 
-현재 시각입니다. JVM 의 user.timezone 대로 사용자의 편의를 위해 rendering 해서 문자열을 보여줍니다.
+;; 현재 시각입니다. JVM 의 user.timezone 대로, `(TimeZone/getDefaults)` 대로,
+;; 사용자의 편의를 위해 rendering 해서 문자열을 보여줍니다.
+
 
 ^:nextjournal.clerk/no-cache
 (defn r1 [sql]
@@ -60,19 +62,19 @@
 
 ;; 여기까지는 mysql 과 mysql cli 클라이언트가 어떻게 동작하는가? 에 대한 이야기였습니다.
 ;;
-;; mysql-connector-j 은 JDBC, 일종의 java interface 모음을 지원하는 라이브러리 입니다.
-;; 꽤 규격화된 interface 를 지원하지만 그 구현체로서 시간처리를 어떻게 할지는
-;; 구현체다마 아주 아주 약간 달라진다고 볼 수 있습니다. 그린랩스에서도 mysql-connector-j
-;; 8.0.22 와 8.0.23 사이에서 달라지 동작을 목격한 적이 있습니다.
+;; mysql-connector-j 은 JDBC 의 구현체입니다.
+;; 규격화된 JDBC interface 를 지원하지만, 그 구현체로서 시간처리를 어떻게 할지는
+;; 구현체마다 달라진다고 볼 수 있습니다. 그린랩스에서도 mysql-connector-j
+;; 8.0.22 와 8.0.23 사이에서 달라진 동작을 주빈님이 목격한 적이 있습니다.
 ;;
-;; 한데 JVM 의 TimeZone 설정, JDK 의 날짜+시간 클래스들, MySQL 의 wire-level protocol,
-;; mysql 의 session timezone 설정이 섞여서 무척 헷갈립니다.
+;; 여기서 부터 JVM 의 TimeZone 설정, JDK 의 날짜+시간 클래스들, MySQL 의 wire-level protocol,
+;; mysql 의 session timezone 설정이 섞여 이해하기가 어렵습니다.
 ;;
 ;; 비교적 설명하기 쉬운 것 부터 알아봅니다. JVM 의 날짜+시간 클래스 중에 권장(?) 클래스 부터 알아봅니다.
 ;;
 ;; java.time.Instant 이 mysql-connector-j 의 문서 [Preserving Time Instant](https://docs.oracle.com/cd/E17952_01/connector-j-8.0-en/connector-j-time-instants.html)
-;; 에서 말하는 instant 를 표현할 때 가장 적합한 클래스라고 가정해봅니다.
-;; 문서 Preserving Time Instant 에서 개념 `Instant` 를 이렇게 설명합니다.
+;; 에서 말하는 instant 를 표현할 때 가장 적합한 클래스라고 가정해봅니다. 문서 Preserving Time Instant 에서
+;; 개념 `Instant` 를 이렇게 설명합니다.
 ;;
 ;; A time instant is a specific moment on a time-line. A time instant is said to be
 ;; preserved when it always refers to the same point in time when its value
@@ -81,12 +83,13 @@
 ;;
 ;; 그리고 java.time.Instant 는 ns `clojure.instant` 도 존재하고
 ;; tagged literal `#inst` 도 존재하니 clojure 에서도
-;; java.time.Instant 를 사용하는 것이 Ideomatic 하다고 쳐 봅니다. next.jdbc [2] 에서도
-;; java.time.Instant 를 관용적으로 잘 처리해줍니다. protocl `SettableParameter` 에서 확장해 줍니다.
+;; java.time.Instant 를 사용하는 것이 Ideomatic 하다고 가정해 봅니다.
+;; next.jdbc [2] 에서도 java.time.Instant 를 관용적으로 잘 처리해줍니다.
+;; protocl `SettableParameter` 에서 확장하는 방법으로 지원합니다.
 ;;
 ;; `java.util.Date` 는 JDK 1.0 부터 존재하던 클래스입니다. 옛 클래스이고 java.time package 의
-;; 어떤 클래스와 딱 1:1 대응된다고 말하기 어려우니 clojure 의 simple 과는 거리가 먼 class 라고 치고
-;; 심각하게 않는 것이 좋다고 칩니다.
+;; 어떤 클래스와 딱 1:1 대응된다고 말하기 어려우니 clojure 의 `Simple` 과는 거리가 먼 class 라고 치고
+;; 심각하게 고려하지 않습니다.
 ;;
 ;; `java.util.Calander` 는 JDK 1.1 에서 탄생했고, `java.util.Date`의 기능의 일부를 좀 더 올바르게
 ;; 구현하는 듯 싶습니다. mysql-connector-j 에서 wire-level bytearray 를 해석하는 코드에서도
@@ -96,22 +99,24 @@
 ;; 개념 `instant` 를 저장하는 용도로 쓰는 클래스입니다.
 ;;
 ;; `java.time.ZoneDateTime` 은 (Instant + TimeZone) 이라고 이해할 수 있습니다.
-;; Instant.atZone(ZoneId) 로 ZonedDateTime 으로 변환할 수 있습니다. 특별히 더 좋을 것이 없어보이니
-;; 프로그램 내부에서는 문자열로 변환하기 직전까지 java.time.Instant 을 value 로 사용하는 것이 좋아보입니다.
+;; 코드 `Instant.atZone(ZoneId)` 로 ZonedDateTime 으로 변환합니다. 더 좋을 것이 없어보이니
+;; 프로그램 내부에서는 문자열로 변환하기 직전까지 java.time.Instant 을 Value 로 사용하는 정책이 좋아보입니다.
 ;; next.jdbc 에서 `java.time.Instant` 를 특별 취급해 주는 것을 보면서 그렇게 생각하게 되었습니다.
 ;;
 ;; 여기까지가 소위 instant date-time types 였습니다.
-;; non-instant date-time types 로 `java.time.LocalDateTime` 가 있습니다. [3]
+;;
+;; non-instant date-time types 로 `java.time.LocalDateTime` 가 있습니다.
+;; 다른 non-instant date-time types 도 있겠지만 고려하지 말아 봅니다. [3]
 ;;
 ;; 문서 `Preserving Time Instant` 에서 발췌하면
+;; 이렇게 instant date-time types 와 non-instant date-time types 를 섞지 말라고 합니다.
 ;;
 ;; Therefore, do not pass instant date-time types (java.util.Calendar, java.util.Date,
 ;; java.time.OffsetDateTime, java.sql.Timestamp) to non-instant date-time types
 ;; (for example, java.sql.DATE, java.time.LocalDate, java.time.LocalTime, java.time.OffsetTime)
 ;; or vice versa, when working with the server.
 ;;
-;; 이렇게 instant date-time types 와 non-instant date-time types 를 섞지 말라고 합니다.
-;;
+;; 섞었을 때 예상하기 무척 어려운 동작이 생긴다고 하고 되도록이면 그러지 않습니다.
 ;;
 ;; mysql-connector-j 8.0.22 부터
 ;; [6.3.11 Datetime types processing](https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-connp-props-datetime-types-processing.html) 에
@@ -122,24 +127,87 @@
 ;; [DateTimeTest.java](https://github.com/mysql/mysql-connector-j/blob/release%2F8.0/src/test/java/testsuite/simple/DateTimeTest.java#L3962-L3964) 에서
 ;; 볼 수 있습니다. [1]
 ;;
-;; 하지만 그렇게 따라가는 것이 좀 어려웠습니다. XXX mysql-connector-j 의 connection property
-;; `connectionTimeZone`, `forceConnectionTimeZoneToSession`, `preserveInstants` property 의
-;; default 값 아래 어떻게 되는지 예제를 보려고 합니다.
+;; 하지만 그렇게 따라가는 것이 좀 어려웠습니다. 그러니 mysql-connector-j 8.0.28 에서 각 property 의
+;; default 값 아래 어떻게 되는지 예제를 보려고 합니다. 신선하이에서는 아무 property 도 설정하지 않으니
+;; default 값으로 씁니다. connectionTimeZone, forceConnectionTimeZoneToSession, preserveInstants 의
+;; default 값은 순서대로 N/A, false, true 입니다.
 ;;
-;; next.jdbc 에서는 날짜, 시간과 관련해서 변환을 최대한 JDBC 레이어에 맡깁니다.
+;; 시험할 때 next.jdbc 를 같이 사용합니다. next.jdbc 에서는 날짜, 시간과 관련해서 변환을
+;; 최대한 JDBC 레이어에 맡깁니다.
 ;; java.time.Instant 를 protocol SettableParameter 에서 지원해 주는 것은
-;; 아주 약간 있는 그리고 next.jdbc 에서 지원해 줄 수 있는 최소한이라 봐야 합니다.
+;; next.jdbc 에서 지원해 줄 수 있는 최대한으로 정말 작은 부분이라고 봐야 합니다.
 ;;
+;; Sean Corfield 가 이런 글을 남긴 적이 있습니다.
+;;
+;; ```
 ;; https://github.com/seancorfield/next-jdbc/issues/73#issuecomment-553021972
-;;
 ;; Different databases have different SQL types for representing
 ;; dates/times, with and without timezones,
 ;; so there is no "one size fits all" that
 ;; next.jdbc could "enforce" for all programs.
+;; ```
+;;
+;; 그러니 이 글에서 next.jdbc 로 시험하지만 거의 영향이 없습니다
+;; mysql server 는 docker 8.0.29 를 사용합니다.
+
+
+;; datetime 필드가 있는 table 을 생성합니다.
 ;;
 
-;; mysql-connector-j 의 connection property
-;; `connectionTimeZone`, `forceConnectionTimeZoneToSession`, `preserveInstants` 에 따라
+;; JVM default 타임존을 #{"Asia/Seoul" "UTC"} 로 선택한 다음
+;; 각각 어떻게 되는지 봅니다.
+;;
+;; "2020-01-01 10:10:10" 을 문자열로,
+;; 그리고 LocalDateTime 으로 만들어서,
+;; 그리고 ZonedDateTime (UTC) 으로 만들어서,
+;; 그리고 그 ZonedDateTime 을 Instant 로 바꿔서,
+;; 그리고 Instant 를 `clojure.core/inst-ms`를 사용해 Long 으로 바꿔서,
+;;
+;; 저장합니다.
+;;
+
+;; select 를 해서 어떻게 나오는지 봅니다.
+;; XXX "UTC" 로 했을 때는
+;;   전부 LocalDateTime 으로 2020-01-01 10:10:10 이 나올 거라 기대하고 있다.
+;; XXX "Asia/Seoul" 로 했을 때도
+;;   전부 LocalDateTime 으로 2020-01-01 10:10:10 이 나올 거라 기대하고 있다.
+
+;; timestamp 필드가 있는 table 을 생성합니다.
+;;
+;; JVM default 타임존을 #{"Asia/Seoul" "UTC"} 로 선택한 다음
+;; 각각 어떻게 되는지 봅니다.
+;;
+;; "2020-01-01 10:10:10" 을 문자열로,
+;; 그리고 LocalDateTime 으로 만들어서,
+;; 그리고 ZonedDateTime (UTC) 으로 만들어서,
+;; 그리고 그 ZonedDateTime (UTC) 을 Instant 로 바꿔서,
+;; 그리고 Instant 를 `clojure.core/inst-ms`를 사용해 Long 으로 바꿔서,
+;;
+;; 저장합니다.
+;;
+;; select 했을 때
+;; XXX Asia/Seoul 은
+;;   1. java.sql.Timestamp 로 2020-01-01 10:10:10+09:00 의 instant 로
+;;   2. java.sql.Timestamp 로 2020-01-01 10:10:10+09:00 의 instant 로
+;;   3. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   4. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   5. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;
+;; 나오기를 기대한다.
+;;
+;; XXX UTC 는
+;;   1. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   2. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   3. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   4. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;   5. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
+;;
+;; 로 나오기를 기대한다.
+;;
+
+
+
+;; -----
 
 ;; java 안에서 java.sql.Timestamp 는 어떻게 동작하게 되나?
 ;;   jst 는 timezone 을 system timezone(== local timezone) 이라고 해석하는 클래스인가?
@@ -157,42 +225,24 @@
 ;; 섞어서 쓰면 예상치 못한 결과가 나올 수 있다고 한다.
 
 
+;; # QnA
 
+;; next.jdbc 에서 prepared statement 에 parameter 로 넣을 때도 next.jdbc 에서 변환을 할까?
+;; 아니다. 그렇게 강제로 변환하는 부분은 없다고 보는 것이 좋다. 전부 mysql-connector-j 에서
+;; 하는 구현에서 만든 변환이라고 봐야 한다.
 
-;; prepared statement 에 parameter 로 들어가는 것도 영향을 주는 요인일까?
+;; 그래서 어떻게 하면 좋을까? recipe 가 있다면 무엇인가?
+;; TBD
 
-;; 내가 쓰고 나서 그래도 답을 하고 싶은 것은
-;;  그래서 mysql timestamp field 이면 jvm 의 zoneddatetime 으로 변환되는 건가?
-;;  혹시 localdatetime 으로 나오는 건가?
-;;  혹시 localdatetime 으로 나오면 jvm 에서 무슨 zone offset 이라고 생각하고 inst 로 바꾸면 되나?
-
-;; 그래서 지금있는 datetime 이나 timestamp 필드에 지금 설정으로 select 를 한다고 하면
-;; jvm 에 무슨 타입이 보이게 되는 건가?
-
-
-;; mysql 에서 timezone offset 이 붙은 리터럴을 8.0.19 부터 지원하기 시작했다.
-;; 이것은 jdbc 의 prepared statement 와 관련이 있을까?
-
-;; mysql connector j 안에서 어떤 변환을 해주고 있을까?
-
-;; next.jdbc 에서는 어떤 변환을 해주려고 시도하고 있나?
-
-
-;; 테이블 생성없이 select 를 바로해서 값을 보자.
-;; CAST 를 하는 것으로 타입이 잘 보이는 거 겠지?
-
-;; 하는 길에 DEFAULT CURRUNT_TIMESTAMP 와 ON UPDATE CURRUNT_TIMESTAMP 의 기능도 시험해보자.
-;; 한데 나중에 글에서는 빼자.
+;; mysql 에서 timezone offset 이 붙은 리터럴을 8.0.19 부터 지원하기 시작했다. 이것은 jdbc 의
+;; prepared statement 와 처리와 연관된 것이 있을까?
 
 ;; 몰랐다가 배운 것은 무엇인가? timestamp 가 어디서 부터 어디까지 표현할 수 있는가?
 
 ;; preserveInstant 옵션이 끼치는 영향은 그래서 뭐라고?
 
 
-;; mysql 의 date and time data types 에 대해서 좀 더 이해하자.
-;; 2차 저작으로 남겨두는 것은 뭐가 좋을까?
-;;
-
+;; # XXX
 ;; 이 파일을 실행하는 아래 환경에서 실행했다.
 ;; JVM 에서 시간을 얻으려면 이렇게 한다.
 
@@ -223,9 +273,8 @@
 ;; mysql 에서 timestamp 를 얻는 방법이 있나?
 ;; session 설정이 다르면 다른 값으로 나오나?
 ;;
-;;
 
-;; mysql 의 date, time, datetime, timestamp 는 무엇인가?
+;; mysql 의 datetime, timestamp 는 무엇인가?
 ;;
 
 ;; JVM 에서 이렇게 얻은 값은 이 설정에 영향을 많이 받습니다.
