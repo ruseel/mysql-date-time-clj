6b59ee92433233e3c7cc0c4a57f26e03747c3463
ruseel <ruseel@gmail.com>
2022-06-24 09:54:45 +0900

    WIP

--- notebooks/mysql.clj
+++ notebooks/mysql.clj
@@ -3,11 +3,11 @@
 (ns mysql
   (:require [nextjournal.clerk :as clerk]
             [next.jdbc :as jdbc]
+            [next.jdbc.date-time]
             [clojure.datafy :refer :all]
             [clojure.java.shell :refer [sh]])
-  (:import [java.time
-            LocalDateTime
-            ZonedDateTime]))
+  (:import [java.util TimeZone]
+           [java.time LocalDateTime ZonedDateTime ZoneId]))
 
 ;; MySQL 과 프로그래밍 언어 사이에서 날짜,시간을 다룰 때 모르는 부분이 많습니다.
 ;; 어떤 환경이 영향을 끼치는지 목록을 만들어 ADR 을 만들 때, 시간을 저장하거나 읽을 때
@@ -42,7 +42,7 @@
 (defn r1 [sql]
   (clerk/html
    [:pre (:out
-          (sh "mysql" "-h" "127.0.0.1" "-u" "root" :in sql :in-enc "UTF8"))]))
+          (sh "mysql" "-h" "127.0.0.1" "-u" "root" "test" :in sql :in-enc "UTF8"))]))
 
 (r1 "select now() as col")
 
@@ -145,7 +145,7 @@
 ;; dates/times, with and without timezones,
 ;; so there is no "one size fits all" that
 ;; next.jdbc could "enforce" for all programs.
-;; ```
+;;  ```
 ;;
 ;; 그러니 이 글에서 next.jdbc 로 시험하지만 거의 영향이 없습니다
 ;; mysql server 는 docker 8.0.29 를 사용합니다.
@@ -153,6 +153,8 @@
 
 ;; datetime 필드가 있는 table 을 생성합니다.
 ;;
+^{:nextjournal.clerk/visibility #{:hide}}
+(r1 "create table datetime_t (id int, dt datetime);")
 
 ;; JVM default 타임존을 #{"Asia/Seoul" "UTC"} 로 선택한 다음
 ;; 각각 어떻게 되는지 봅니다.
@@ -166,14 +168,50 @@
 ;; 저장합니다.
 ;;
 
+(def db {:dbtype "mysql" :dbname "test" :user "root"})
+
+(def str-dt "2020-01-01 10:10:10")
+(def ldt (java.time.LocalDateTime/of 2020 1 1 10 10 10))
+(def zdt (java.time.ZonedDateTime/of 2020 1 1 10 10 10 0 (ZoneId/of "UTC")))
+(def inst (.toInstant zdt))
+
+^:nextjournal.clerk/no-cache
+(defn test-dt-t []
+  (doall
+   (for [[id v] [[1 str-dt]
+                 [2 ldt]
+                 [3 zdt]
+                 [4 inst]]]
+     (next.jdbc/execute!
+      db
+      ["insert into datetime_t(id, dt) values (?, ?)" id v]))))
+
+
+^:nextjournal.clerk/no-cache
+(TimeZone/setDefault (TimeZone/getTimeZone "UTC"))
+
+^:nextjournal.clerk/no-cache
+(test-dt-t)
+
+^:nextjournal.clerk/no-cache
+(TimeZone/setDefault (TimeZone/getTimeZone "Asia/Seoul"))
+
+^:nextjournal.clerk/no-cache
+(test-dt-t)
+
+
 ;; select 를 해서 어떻게 나오는지 봅니다.
-;; XXX "UTC" 로 했을 때는
-;;   전부 LocalDateTime 으로 2020-01-01 10:10:10 이 나올 거라 기대하고 있다.
-;; XXX "Asia/Seoul" 로 했을 때도
-;;   전부 LocalDateTime 으로 2020-01-01 10:10:10 이 나올 거라 기대하고 있다.
+
+^:nextjournal.clerk/no-cache
+(next.jdbc/execute! db ["select * from datetime_t"])
+
+;; 주목할 부분은
 
 ;; timestamp 필드가 있는 table 을 생성합니다.
 ;;
+^{:nextjournal.clerk/visibility #{:hide}}
+(r1 "create table timestamp_t (id int, ts timestamp);")
+
 ;; JVM default 타임존을 #{"Asia/Seoul" "UTC"} 로 선택한 다음
 ;; 각각 어떻게 되는지 봅니다.
 ;;
@@ -181,106 +219,66 @@
 ;; 그리고 LocalDateTime 으로 만들어서,
 ;; 그리고 ZonedDateTime (UTC) 으로 만들어서,
 ;; 그리고 그 ZonedDateTime (UTC) 을 Instant 로 바꿔서,
-;; 그리고 Instant 를 `clojure.core/inst-ms`를 사용해 Long 으로 바꿔서,
 ;;
 ;; 저장합니다.
-;;
-;; select 했을 때
-;; XXX Asia/Seoul 은
-;;   1. java.sql.Timestamp 로 2020-01-01 10:10:10+09:00 의 instant 로
-;;   2. java.sql.Timestamp 로 2020-01-01 10:10:10+09:00 의 instant 로
-;;   3. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   4. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   5. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;
-;; 나오기를 기대한다.
-;;
-;; XXX UTC 는
-;;   1. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   2. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   3. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   4. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;   5. java.sql.Timestamp 로 2020-01-01 10:10:10+00:00 의 instant 로
-;;
-;; 로 나오기를 기대한다.
-;;
 
 
+^:nextjournal.clerk/no-cache
+(do
+  (defn insert-to-ts-t []
+    (doall
+     (for [[id v] [[1 str-dt]
+                   [2 ldt]
+                   [3 zdt]
+                   [4 inst]]]
+       (next.jdbc/execute!
+        db
+        ["insert into timestamp_t(id, ts) values (?, ?)" id v]))))
 
-;; -----
+  (TimeZone/setDefault (TimeZone/getTimeZone "UTC"))
+  (insert-to-ts-t)
 
-;; java 안에서 java.sql.Timestamp 는 어떻게 동작하게 되나?
-;;   jst 는 timezone 을 system timezone(== local timezone) 이라고 해석하는 클래스인가?
-;; 특히나 next.jdbc 의 SettableParameter 가 뭐라고 생각하고 동작하게 되는 것인가?
+  (TimeZone/setDefault (TimeZone/getTimeZone "Asia/Seoul"))
+  (insert-to-ts-t))
 
+;; next.jdbc + mysql-connector-j 를 통해서 조회했을 때는 어떤 객체가 나오는지 봅니다.
 
-;; MySQL DataType 중에 DATETIME, TIMESTAMP 는 어떻게 차이가 나나?
-;; Greenlabs ADR 에서
-
-;; mysql-connector-j 에서는 이 설명에 나오는 부분이 아주 심하게 영향을 끼친다.
-;; https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-time-instants.html
-;;
-
-;; instant datatype 과 non-instant datatype 의 차이가 존재한다.
-;; 섞어서 쓰면 예상치 못한 결과가 나올 수 있다고 한다.
+^:nextjournal.clerk/no-cache
+(next.jdbc/execute! db ["select * from timestamp_t"])
 
 
 ;; # QnA
 
 ;; next.jdbc 에서 prepared statement 에 parameter 로 넣을 때도 next.jdbc 에서 변환을 할까?
+;;
 ;; 아니다. 그렇게 강제로 변환하는 부분은 없다고 보는 것이 좋다. 전부 mysql-connector-j 에서
 ;; 하는 구현에서 만든 변환이라고 봐야 한다.
 
 ;; 그래서 어떻게 하면 좋을까? recipe 가 있다면 무엇인가?
+;;
 ;; TBD
 
-;; mysql 에서 timezone offset 이 붙은 리터럴을 8.0.19 부터 지원하기 시작했다. 이것은 jdbc 의
-;; prepared statement 와 처리와 연관된 것이 있을까?
-
-;; 몰랐다가 배운 것은 무엇인가? timestamp 가 어디서 부터 어디까지 표현할 수 있는가?
-
-;; preserveInstant 옵션이 끼치는 영향은 그래서 뭐라고?
-
-
-;; # XXX
-;; 이 파일을 실행하는 아래 환경에서 실행했다.
-;; JVM 에서 시간을 얻으려면 이렇게 한다.
-
-(System/getProperty "user.timezone")
-(ZonedDateTime/now)
-(LocalDateTime/now)
-
-;; MySQL 로 접속해 보자
-
-(def db {:dbtype "mysql" :dbname "test" :user "root"})
-(first (vals (jdbc/execute-one! db ["select now()"])))
-
-;; MySQL 에서 now() 로 얻은 값이 JVM 에서 LocalDateTime 으로 얻어집니다.
-
-;; JVM 의 영향없이 mysql cli 로 직접 접속해서 시험해봅니다.
+;; mysql 에서 timezone offset 이 붙은 리터럴을 8.0.19 부터 지원하기 시작했다. 이 기능이
+;; jdbc 의 prepared statement 와 setParameter 처리 후에 일어나는 변환들과 연관이 있을까?
 ;;
+;; 없다.
 ;;
 
-(defn run-1 [sql]
-  (println
-   (:out
-    (sh "mysql" "-h" "127.0.0.1" "-u" "root"
-        :in "select now()" :in-enc "UTF8"))))
-
-(run-1 "select now()")
-
-
-;; mysql 에서 timestamp 를 얻는 방법이 있나?
-;; session 설정이 다르면 다른 값으로 나오나?
+;; 그래서 작성 과정 중에 새로 배운 것은 무엇인가?
+;;
+;; TBD
 ;;
 
-;; mysql 의 datetime, timestamp 는 무엇인가?
+;; preserveInstant 옵션이 끼치는 영향은 그래서 무엇인가?
 ;;
+;; TBD
 
-;; JVM 에서 이렇게 얻은 값은 이 설정에 영향을 많이 받습니다.
+
+;; # 참고
 ;;
-;; https://dev.mysql.com/doc/connector-j/8.0/en/connector-j-time-instants.html
-
+;; * [철도 시간표가 유닉스 시간이 되기까지](https://parksb.github.io/article/39.html)
+;;
+;; # 주석
 
 ;; [1] [DateTimeTest.java](https://github.com/mysql/mysql-connector-j/blob/dd61577595edad45c398af508cf91ad26fc4144f/src/test/java/testsuite/simple/DateTimeTest.java#L4405) 에서
 ;; testSymmetricInstantRetrieval 함수를 보면
